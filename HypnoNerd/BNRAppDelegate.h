18041a85d6c4c8257a3a1a0000ce9042627138b3
Kevin Quach <kevin.quach@gmail.com>
2014-10-18 11:19:43 -0700

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Kevin Quach on 10/18/14.
+//  Copyright (c) 2014 Kevin Quach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
